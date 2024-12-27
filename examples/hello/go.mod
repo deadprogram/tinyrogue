8b61a7d6e95f0cf1e97d425fa053b0206cdace40
deadprogram <ron@hybridgroup.com>
2024-12-27 09:44:54 +0100

    examples: update Hello to use latest of everything
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/hello/go.mod
+++ examples/hello/go.mod
@@ -6,7 +6,7 @@ replace github.com/deadprogram/tinyrogue => ../../
 
 require (
 	github.com/deadprogram/tinyrogue v0.0.0-00010101000000-000000000000
-	github.com/firefly-zero/firefly-go v0.7.1
+	github.com/firefly-zero/firefly-go v0.8.0
 )
 
 require (
--- examples/hello/main.go
+++ examples/hello/main.go
@@ -14,25 +14,44 @@ func init() {
 var game *tinyrogue.Game
 
 func boot() {
+	firefly.LogDebug("Boot")
 	game = tinyrogue.NewGame()
 
-	floorImage := firefly.LoadFile("floor").Image()
+	floorImage := firefly.LoadFile("floor", nil).Image()
 	game.Images["floor"] = &floorImage
 
-	wallImage := firefly.LoadFile("wall").Image()
+	wallImage := firefly.LoadFile("wall", nil).Image()
 	game.Images["wall"] = &wallImage
 
-	playerImage := firefly.LoadFile("player").Image()
+	playerImage := firefly.LoadFile("player", nil).Image()
 	game.Images["player"] = &playerImage
 	player := tinyrogue.NewPlayer()
 	player.SetImage(game.Images["player"])
+	player.SetSpeed(10)
 
+	monsterImage := firefly.LoadFile("monster", nil).Image()
+	game.Images["monster"] = &monsterImage
+	monster := tinyrogue.NewCreature()
+	monster.SetImage(game.Images["monster"])
+	monster.SetSpeed(60)
+
+	firefly.LogDebug("set data")
 	game.SetData(tinyrogue.NewGameData(16, 10))
+
+	firefly.LogDebug("set map")
 	game.SetMap(tinyrogue.NewGameMap())
+
+	firefly.LogDebug("set player")
 	game.SetPlayer(player)
+	game.AddCreature(monster)
 
 	// set player initial position
-	player.MoveTo(&tinyrogue.Position{X: 1, Y: 1})
+	entrance := &tinyrogue.Position{X: 1, Y: 1}
+	player.MoveTo(entrance)
+
+	// set monster initial position
+	monsterPos := &tinyrogue.Position{X: 5, Y: 5}
+	monster.MoveTo(monsterPos)
 }
 
 func update() {
