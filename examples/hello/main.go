70465067443a48f7f87a8e5a954b80e9320f35a5
deadprogram <ron@hybridgroup.com>
2024-12-28 02:31:24 +0100

    examples: changes to hello example for simplicity
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -24,28 +24,15 @@ func boot() {
 
 	playerImage := firefly.LoadFile("player", nil).Image()
 	player := tinyrogue.NewPlayer("Player", &playerImage, 5)
-	player.ViewRadius = 4
-
-	monsterImage := firefly.LoadFile("monster", nil).Image()
-	monster := tinyrogue.NewCreature("Monster", &monsterImage, 60)
-	monster.SetBehavior(tinyrogue.CreatureApproach)
 
 	game.SetData(tinyrogue.NewGameData(16, 10, 16, 16))
 	game.SetMap(tinyrogue.NewGameMap())
-	game.UseFOV = true
 
 	game.SetPlayer(player)
-	game.AddCreature(monster)
-
-	game.SetActionSystem(&CombatSystem{})
 
 	// set player initial position
 	entrance := tinyrogue.Position{X: 1, Y: 1}
 	player.MoveTo(entrance)
-
-	// set monster initial position
-	monsterPos := tinyrogue.Position{X: 12, Y: 7}
-	monster.MoveTo(monsterPos)
 }
 
 func update() {
@@ -54,11 +41,4 @@ func update() {
 
 func render() {
 	game.Render()
-}
-
-type CombatSystem struct {
-}
-
-func (ca *CombatSystem) Action(attacker tinyrogue.Character, defender tinyrogue.Character) {
-	firefly.LogDebug(attacker.Name() + " is attacking " + defender.Name())
 }
